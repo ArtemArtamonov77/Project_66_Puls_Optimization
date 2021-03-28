1d365dd855c5113118dcfa4636d717db08227940
Артамонов Артём Александрович <ArtemWebMastering@gmail.com>
2021-03-28 10:42:19 +0300

    оптимизация - перенесли все css блоки в папку libs,настроили сжатие img и html через дополнения gulp c копированием всех файлов в папку dist

---
+++ ex
@@ -0,0 +1,17 @@
+[33mcommit 3a1eb49748ad7cb13418b0e4ad4d4e7651c029f0[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: Артамонов Артём Александрович <ArtemWebMastering@gmail.com>
+Date:   Sat Mar 27 18:34:36 2021 +0300
+
+    убрал анимацию через медия
+
+[33mcommit 95bdf70e2683762ac25d066ee8ed38b9670a4f1a[m
+Author: Артамонов Артём Александрович <ArtemWebMastering@gmail.com>
+Date:   Sat Mar 27 18:33:49 2021 +0300
+
+    убрал анимацию через медия
+
+[33mcommit a63ac0ce08a0bfe7b16a85e3b228f69e955f7f84[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: Артамонов Артём Александрович <ArtemWebMastering@gmail.com>
+Date:   Sat Mar 27 15:42:14 2021 +0300
+
+    firstcommit
--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,15 @@ const sass        = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');/* сжатие изображений */
+const htmlmin = require('gulp-htmlmin'); /* сжатие html */
+
 //задача автообновления страницы браузера, baseDir: "src" директория рабочих файлов проекта
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
@@ -27,12 +30,50 @@ gulp.task('styles', function() {
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
-
+/* задача слежения за файлами html */
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+});
+/* Задача минизации файлов html и последующее копирование их в папку dist */
+gulp.task('html', function () {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+/* Задача копирования скрипт файлов в папку dist */
+gulp.task('scripts', function () {
+    return gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest("dist/js"));
+});
+
+/* Задача копирования папки шрифтов в папку dist */
+gulp.task('fonts', function () {
+    return gulp.src('src/fonts/**/*.*')
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+/* Задача копирования папки icons в папку dist */
+gulp.task('icons', function () {
+    return gulp.src('src/icons/**/*.*')
+        .pipe(gulp.dest("dist/icons"));
+});
+
+/* Задача копирования папки mailer в папку dist */
+gulp.task('mailer', function () {
+    return gulp.src('src/mailer/**/*.*')
+        .pipe(gulp.dest("dist/mailer"));
+});
+
+/* Задача зжатия и копирования папки img в папку dist */
+gulp.task('images', function () {
+    return gulp.src('src/img/**/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts', 'icons', 'mailer', 'images'));
