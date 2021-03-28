6317ac3b85c6a82f2d48f5309e2e8a3584ef85d4
Артамонов Артём Александрович <ArtemWebMastering@gmail.com>
2021-03-28 19:51:38 +0300

    оптимизация - перенесли все css блоки в папку libs,настроили сжатие img и html через дополнения gulp c копированием всех файлов в папку dist, максимально переделал онлайн скрипты в офлайн

--- dist/js/script.js
+++ dist/js/script.js
@@ -26,38 +26,40 @@ $(document).ready(function(){
   }); */
 
   /* Tiny-slider */
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: false,
-    /* responsive: {
-      640: {
-        edgePadding: 20,
-        gutter: 20,
-        items: 1
-      },
-      700: {
-        gutter: 30
-      },
-      900: {
-        items: 1,
-        fixedWidth: 1000
-      }
-    }, */
-    controlsText: [
-        '<img src="icons/left.svg">',
-        '<img src="icons/right.svg">'
-    ]
-  });
-  document.querySelector('.prev').addEventListener('click', function () {
-      slider.goTo('prev');
+  
+  
+  const slider = tns({
+      container: '.carousel__inner',
+      items: 1,
+      slideBy: 'page',
+      autoplay: false,
+      controls: false,
+      nav: false,
+      /* responsive: {
+        640: {
+          edgePadding: 20,
+          gutter: 20,
+          items: 1
+        },
+        700: {
+          gutter: 30
+        },
+        900: {
+          items: 1,
+          fixedWidth: 1000
+        }
+      }, */
+      controlsText: [
+          '<img src="icons/left.svg">',
+          '<img src="icons/right.svg">'
+      ]
     });
-  document.querySelector('.next').addEventListener('click', function () {
-      slider.goTo('next');
-  });
+    document.querySelector('.prev').addEventListener('click', function () {
+        slider.goTo('prev');
+      });
+    document.querySelector('.next').addEventListener('click', function () {
+        slider.goTo('next');
+    });
 /* скрипт работы табов */
 /* $(document).ready(function(){
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
